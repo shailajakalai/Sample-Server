0a9569d24facd2a1c22ecea7427c31179633d670
shailajakalai <121797891+shailajakalai@users.noreply.github.com>
2023-05-11 19:33:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 9080 ;
 const config = require('config')
 console.log(config);
 
